56f42a6b41157f9d93f3edb5c8be2b34aec0af85
Randy Herasme <flatironschool@Flatirons-MacBook-Air.local>
2018-10-29 13:13:20 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hey world!"
+  end
+
+  get '/name' do
+    "My name is Randy"
+  end
+
+  get '/hometown' do
+    "My hometown is The Bronx"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Lights Please"
+  end
+
 end
